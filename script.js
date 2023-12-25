50fbc00279675f8c5534130e42f917f136bfe316
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-25 15:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1.length === 0 || s2.length === 0){
+		return "";
+	}
+
+	let ans = "";
+
+	ans += s1.charAt(0);
+	ans += s2.charAt(0);
+
+	return ans;
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+alert(shortcut(s1, s2)); 
